3327e0503d77698235bfdb14ae530e828335fb46
Johnny Preyer <jpreyer@gmail.com>
2014-10-29 00:11:22 -0400

    changing BOOTSTRAP_THEME to spacelab

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,7 +17,7 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 THEME = '/home/jpreyer/Projects/pelican-bootstrap3/'
-BOTOSTRAP_THEME = 'spacelab'
+BOOTSTRAP_THEME = 'spacelab'
 
 # Blogroll
 #LINKS = (('Pelican', 'http://getpelican.com/'),
